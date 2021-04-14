34ee8ec5d8283869b2664637706d18745e97e4d5
Jade Lapuz <jadelapuz@TEMAC-002.local>
2021-04-15 06:59:07 +0800

    initial commit

---
+++ Fast&Furios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fast&Furios
+//
+//  Created by Jade Lapuz on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
