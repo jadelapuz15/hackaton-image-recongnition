678703b7e08ab8e414e801aabde9d3664b5ed379
Jade Lapuz <jadelapuz@TEMAC-002.local>
2021-04-15 07:39:17 +0800

    added vision api

--- Fast&Furios/ViewController.swift
+++ Fast&Furios/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
+    
+    
+    @IBAction func checkTextImage(_ sender: UIButton) {
+        
+          // Get the CGImage on which to perform requests.
+          guard let cgImage = sender.backgroundImage(for: .normal)?.cgImage else { return }
 
+          // Create a new image-request handler.
+          let requestHandler = VNImageRequestHandler(cgImage: cgImage)
 
+          // Create a new request to recognize text.
+          let request = VNRecognizeTextRequest { (request, error) in
+              guard let observations =
+                      request.results as? [VNRecognizedTextObservation] else {
+                  return
+              }
+              let recognizedStrings = observations.compactMap { observation in
+                  // Return the string of the top VNRecognizedText instance.
+                  return observation.topCandidates(1).first?.string
+              }
+              
+            self.label.text = recognizedStrings.joined(separator: "\n")
+            print(recognizedStrings)
+          }
+
+          do {
+              // Perform the text-recognition request.
+              try requestHandler.perform([request])
+          } catch {
+              print("Unable to perform the requests: \(error).")
+          }
+    }
 }
 
