05486ebafe2212a1aa949da2cb5826be565fe0b4
Jade Lapuz <jadelapuz@TEMAC-002.local>
2021-04-15 09:22:00 +0800

    added scanner

--- Fast&Furios/ViewController.swift
+++ Fast&Furios/ViewController.swift
@@ -7,46 +7,142 @@
 
 import UIKit
 import Vision
+import VisionKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
     @IBOutlet weak var label: UILabel!
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
     }
     
-    
-    @IBAction func checkTextImage(_ sender: UIButton) {
+    @IBAction func takePhoto(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
         
-          // Get the CGImage on which to perform requests.
-          guard let cgImage = sender.backgroundImage(for: .normal)?.cgImage else { return }
-
-          // Create a new image-request handler.
-          let requestHandler = VNImageRequestHandler(cgImage: cgImage)
-
-          // Create a new request to recognize text.
-          let request = VNRecognizeTextRequest { (request, error) in
-              guard let observations =
-                      request.results as? [VNRecognizedTextObservation] else {
-                  return
-              }
-              let recognizedStrings = observations.compactMap { observation in
-                  // Return the string of the top VNRecognizedText instance.
-                  return observation.topCandidates(1).first?.string
-              }
-              
-            self.label.text = recognizedStrings.joined(separator: "\n")
-            print(recognizedStrings)
-          }
-
-          do {
-              // Perform the text-recognition request.
-              try requestHandler.perform([request])
-          } catch {
-              print("Unable to perform the requests: \(error).")
-          }
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let documentCameraViewController = VNDocumentCameraViewController()
+            documentCameraViewController.delegate = self
+            present(documentCameraViewController, animated: true)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallery()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have permission to access gallery.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func processImage(image: UIImage?) {
+        
+        DispatchQueue.main.async {
+            self.loadingView.startAnimating()
+            self.imageView.image = image
+        }
+       
+        // Get the CGImage on which to perform requests.
+        guard let cgImage = image?.cgImage else { return }
+        
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest { (request, error) in
+            guard let observations =
+                    request.results as? [VNRecognizedTextObservation] else {
+                return
+            }
+            let recognizedStrings = observations.compactMap { observation in
+                // Return the string of the top VNRecognizedText instance.
+                return observation.topCandidates(1).first?.string
+            }
+            
+            DispatchQueue.main.async {
+                self.loadingView.stopAnimating()
+            }
+            
+            guard !recognizedStrings.isEmpty else {
+                let alert = UIAlertController(title: "Error", message: "No text was recognized on the image", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                    self.imageView.image = nil
+                }))
+                
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.label.text = recognizedStrings.joined(separator: "\n")
+            }
+        }
+        
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, VNDocumentCameraViewControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[.originalImage] as? UIImage else {
+           return
+        }
+        
+        imageView.image = pickedImage
+        picker.dismiss(animated: true, completion: {
+            self.processImage(image: pickedImage)
+        })
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        controller.dismiss(animated: true) {
+            DispatchQueue.global(qos: .userInitiated).async {
+                for pageNumber in 0 ..< scan.pageCount {
+                    let image = scan.imageOfPage(at: pageNumber)
+                    self.processImage(image: image)
+                }
+            }
+        }
+    }
+}
