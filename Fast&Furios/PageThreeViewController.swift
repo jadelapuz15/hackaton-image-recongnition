65feaeff76709ff29b3e421a387e583b76328fb8
Aldwin <aldavid476@gmail.com>
2021-04-15 12:46:51 +0800

    Page three on checked and rounded borders

--- Fast&Furios/PageThreeViewController.swift
+++ Fast&Furios/PageThreeViewController.swift
@@ -11,11 +11,22 @@ class PageThreeViewController: UIViewController {
     @IBOutlet weak var cb1: CheckBox!
     
     @IBOutlet weak var cb2: CheckBox!
+    @IBAction func onCb1Checked(_ sender: Any) {
+        cb1.isChecked = true
+        cb2.isChecked = false
+    }
+    @IBAction func onCb2Checked(_ sender: Any) {
+        
+        cb1.isChecked = false
+        cb2.isChecked = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         cb1.style = .circle
         cb2.style = .circle
+        cb1.borderStyle = .rounded
+        cb2.borderStyle = .rounded
         // Do any additional setup after loading the view.
     }
     
